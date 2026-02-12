8bd23ae91c04c0fc25c02f668b97ea3ecc4c8ac9
OceanCing <3419311969@qq.com>
2026-02-12 13:14:57 +0800

    Replenish update() function in Chassis Controller

--- src/rm2_controllers/rm2_chassis_controllers/include/rm2_chassis_controllers/chassis_base.h
+++ src/rm2_controllers/rm2_chassis_controllers/include/rm2_chassis_controllers/chassis_base.h
@@ -47,7 +47,7 @@ public:
   controller_interface::InterfaceConfiguration command_interface_configuration() const override;
   controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-  
+  controller_interface::return_type update(const rclcpp::Time& time, const rclcpp::Duration& period) override;
 protected:
   template<typename T>
   std::unordered_map<std::string, size_t> buildInterfaceIndexMap(T&& interfaces) {
