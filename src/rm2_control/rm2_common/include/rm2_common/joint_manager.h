f4cf314a34f3e2e02df52b0e043fe20c4c7259bc
nesc <raymond059@qq.com>
2026-03-16 18:43:47 +0800

    Enhanced readability

--- src/rm2_control/rm2_common/include/rm2_common/joint_manager.h
+++ src/rm2_control/rm2_common/include/rm2_common/joint_manager.h
@@ -5,32 +5,61 @@
 #pragma once
 
 #include <optional>
-#include <controller_interface/controller_interface.hpp>
 #include <unordered_map>
+#include <hardware_interface/types/hardware_interface_type_values.hpp>
+#include <string>
+#include <vector>
+#include <functional>
+#include <hardware_interface/loaned_state_interface.hpp>
+#include <hardware_interface/loaned_command_interface.hpp>
 
-namespace joint_manager 
+namespace rm2_common
 {
 class JointHandle
 {
 public:
-  explicit JointHandle(const std::string& name);
+  explicit JointHandle(const std::string& name) : name_(name) {}
   ~JointHandle() = default;
 
   void bind_interfaces(
     std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> pos,
     std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> vel,
     std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> effort,
-    std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> cmd);
+    std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> cmd)
+  {
+    pos_state = pos;
+    vel_state = vel;
+    effort_state = effort;
+    cmd_interface = cmd;
+  }
 
-  void read();
+  void read()
+  {
+    if (pos_state) cached_pos = pos_state->get().get_value();
+    if (vel_state) cached_vel = vel_state->get().get_value();
+    if (effort_state) cached_effort = effort_state->get().get_value();
+  }
   
-  inline double getVelocity() const;
-  inline double getPosition() const;
-  inline double getEffort() const;
-  inline double getCommand() const;
-  inline std::string getName() const;
+  inline double getVelocity() const { return cached_vel; }
+  inline double getPosition() const { return cached_pos; }
+  inline double getEffort() const { return cached_effort; }
+  inline double getCommand() const
+  {
+    if (cmd_interface)
+    {
+      return cmd_interface->get().get_value();
+    }
+    return 0.0;
+  }
+  inline std::string getName() const { return name_; }
   
-  void setCommand(double command);
+  void setCommand(double command)
+  {
+    if (cmd_interface) 
+    {
+      cmd_interface->get().set_value(command);
+    }
+  }
 
 private:
   std::string name_;
@@ -47,195 +76,138 @@ class JointManager
 {
 public:
   JointManager() = default;
-  explicit JointManager(std::vector<std::string> names);
+  explicit JointManager(std::vector<std::string> names)
+  {
+    for (const auto& name : names)
+    {
+      joints_.emplace_back(name);
+    }
+  }
   ~JointManager() = default;
 
-  void init(std::vector<std::string> names);
-  void add_joint(std::string name);
+  void init(std::vector<std::string> names)
+  {
+    joints_.clear();
+    for (const auto& name : names)
+    {
+      joints_.emplace_back(name);
+    }
+  }
   
-  size_t size() const;
-  std::vector<std::string> get_names() const;
+  void add_joint(std::string name) { joints_.emplace_back(name); }
   
-  void read_all();
-  void write_all(const std::vector<double>& commands);
+  size_t size() const { return joints_.size(); }
+  
+  std::vector<std::string> get_names() const 
+  {
+    std::vector<std::string> names;
+    names.reserve(joints_.size());
+    for (const auto& joint : joints_) { names.push_back(joint.getName()); }
+    return names;
+  }
+  
+  void read_all() 
+  {
+    for (auto & joint : joints_) 
+    {
+      joint.read();
+    }
+  }
+  
+  void write_all(const std::vector<double>& commands) 
+  {
+    for (size_t i = 0; i < joints_.size(); ++i) {
+      joints_[i].setCommand(commands[i]);
+    }
+  }
 
-  // todo: avoid copy
-  // todo：make it more elegrant
   void bind_all(std::vector<hardware_interface::LoanedStateInterface>& state_interfaces,
-                std::vector<hardware_interface::LoanedCommandInterface>& command_interfaces);
+                std::vector<hardware_interface::LoanedCommandInterface>& command_interfaces)
+  {
+    std::unordered_map<std::string, std::reference_wrapper<hardware_interface::LoanedStateInterface>> state_map;
+    for (auto& it : state_interfaces) {
+      state_map.emplace(it.get_name(), std::ref(it));
+    }
 
-  std::vector<std::string> get_command_interface_names() const;
-  std::vector<std::string> get_state_interface_names() const;
+    std::unordered_map<std::string, std::reference_wrapper<hardware_interface::LoanedCommandInterface>> cmd_map;
+    for (auto& it : command_interfaces) {
+      cmd_map.emplace(it.get_name(), std::ref(it));
+    }
+
+    for (auto & joint : joints_) 
+    {
+      const std::string name = joint.getName();
+      
+      std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> pos_opt = std::nullopt;
+      auto pos_it = state_map.find(name + "/position");
+      if (pos_it != state_map.end()) {
+        pos_opt = std::make_optional(pos_it->second);
+      }
+
+      std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> vel_opt = std::nullopt;
+      auto vel_it = state_map.find(name + "/velocity");
+      if (vel_it != state_map.end()) {
+        vel_opt = std::make_optional(vel_it->second);
+      }
+
+      std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> effort_opt = std::nullopt;
+      auto effort_it = state_map.find(name + "/effort");
+      if (effort_it != state_map.end()) {
+        effort_opt = std::make_optional(effort_it->second);
+      }
+
+      std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> cmd_opt = std::nullopt;
+      auto cmd_it = cmd_map.find(name + "/effort");
+      if (cmd_it != cmd_map.end()) {
+        cmd_opt = std::make_optional(cmd_it->second);
+      }
+
+      // todo yaml
+      joint.bind_interfaces(pos_opt, vel_opt, effort_opt, cmd_opt);
+    }
+  }
+
+  std::vector<std::string> get_command_interface_names() const
+  {
+    std::vector<std::string> names;
+    for (const auto & joint : joints_) 
+    {
+      for (const auto & type : allowed_command_types_) 
+      {
+        names.push_back(joint.getName() + "/" + type);
+      }
+    }
+    return names;
+  }
+  
+  std::vector<std::string> get_state_interface_names() const 
+  {
+    std::vector<std::string> names;
+    for (const auto & joint : joints_) 
+    {
+      for (const auto & type : allowed_state_types_) 
+      {
+        names.push_back(joint.getName() + "/" + type);
+      }
+    }
+    return names;
+  }
   
   auto begin() { return joints_.begin(); }
   auto end() { return joints_.end(); }
   auto begin() const { return joints_.begin(); }
   auto end() const { return joints_.end(); }
 
-  JointHandle& operator[](size_t index);
-  const JointHandle& operator[](size_t index) const;
+  JointHandle& operator[](size_t index) { return joints_.at(index); }
+  const JointHandle& operator[](size_t index) const { return joints_.at(index); }
 
 private:
   std::vector<JointHandle> joints_;
-  std::vector<std::string> allowed_command_types_ = {"effort"};
-  std::vector<std::string> allowed_state_types_ = {"position", "velocity", "effort"};
-  // Maybe we should use yaml to set allowed command types
+  std::vector<std::string> allowed_command_types_ = {hardware_interface::HW_IF_EFFORT};
+  std::vector<std::string> allowed_state_types_ = {
+     hardware_interface::HW_IF_POSITION,
+     hardware_interface::HW_IF_VELOCITY,
+     hardware_interface::HW_IF_EFFORT
+   };
 };
-
-inline JointHandle::JointHandle(const std::string& name) : name_(name) {}
-
-inline void JointHandle::bind_interfaces(
-  std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> pos,
-  std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> vel,
-  std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> effort,
-  std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> cmd) 
-{
-  pos_state = pos;
-  vel_state = vel;
-  effort_state = effort;
-  cmd_interface = cmd;
-}
-
-inline void JointHandle::read() 
-{
-  if (pos_state) cached_pos = pos_state->get().get_value();
-  if (vel_state) cached_vel = vel_state->get().get_value();
-  if (effort_state) cached_effort = effort_state->get().get_value();
-}
-
-inline double JointHandle::getVelocity() const {return cached_vel;}
-inline double JointHandle::getPosition() const {return cached_pos;}
-inline double JointHandle::getEffort() const {return cached_effort;}
-inline std::string JointHandle::getName() const {return name_;}
-
-inline double JointHandle::getCommand() const
-{
-  if (cmd_interface)
-  {
-    return cmd_interface->get().get_value();
-  }
-  return 0.0;
-}
- 
-inline void JointHandle::setCommand(double command)
-{
-  if (cmd_interface) 
-  {
-    cmd_interface->get().set_value(command);
-  }
-}
-
-inline JointManager::JointManager(std::vector<std::string> names)
-{
-  for (const auto& name : names)
-  {
-    joints_.emplace_back(name);
-  }
-}
-
-inline void JointManager::init(std::vector<std::string> names)
-{
-  joints_.clear();
-  for (const auto& name : names)
-  {
-    joints_.emplace_back(name);
-  }
-}
-
-inline void JointManager::add_joint(std::string name) {joints_.emplace_back(name);}
-
-inline size_t JointManager::size() const { return joints_.size(); }
-
-inline std::vector<std::string> JointManager::get_names() const {
-  std::vector<std::string> names;
-  names.reserve(joints_.size());
-  for (const auto& joint : joints_) { names.push_back(joint.getName()); }
-  return names;
-}
-
-inline void JointManager::read_all() {
-  for (auto & joint : joints_) 
-  {
-    joint.read();
-  }
-}
-
-inline void JointManager::write_all(const std::vector<double>& commands) 
-{
-  for (size_t i = 0; i < joints_.size(); ++i) {
-    joints_[i].setCommand(commands[i]);
-  }
-}
-
-inline void JointManager::bind_all(std::vector<hardware_interface::LoanedStateInterface>& state_interfaces,
-              std::vector<hardware_interface::LoanedCommandInterface>& command_interfaces)
-{
-  std::unordered_map<std::string, std::reference_wrapper<hardware_interface::LoanedStateInterface>> state_map;
-  for (auto& it : state_interfaces) {
-    state_map.emplace(it.get_name(), std::ref(it));
-  }
-
-  std::unordered_map<std::string, std::reference_wrapper<hardware_interface::LoanedCommandInterface>> cmd_map;
-  for (auto& it : command_interfaces) {
-    cmd_map.emplace(it.get_name(), std::ref(it));
-  }
-
-  for (auto & joint : joints_) 
-  {
-    const std::string name = joint.getName();
-    auto get_state = [&](const std::string& type) -> std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> {
-      auto it = state_map.find(name + "/" + type);
-      return (it != state_map.end()) ? std::make_optional(it->second) : std::nullopt;
-    };
-
-    auto get_cmd = [&](const std::string& type) -> std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> {
-      auto it = cmd_map.find(name + "/" + type);
-      return (it != cmd_map.end()) ? std::make_optional(it->second) : std::nullopt;
-    };
-
-    // todo yaml
-    joint.bind_interfaces(
-      get_state("position"),
-      get_state("velocity"),
-      get_state("effort"),
-      get_cmd("effort")
-    );
-  }
-}
-
-inline std::vector<std::string> JointManager::get_command_interface_names() const
-{
-  std::vector<std::string> names;
-  for (const auto & joint : joints_) 
-  {
-    for (const auto & type : allowed_command_types_) 
-    {
-      names.push_back(joint.getName() + "/" + type);
-    }
-  }
-  return names;
-}
-
-inline std::vector<std::string> JointManager::get_state_interface_names() const 
-{
-  std::vector<std::string> names;
-  for (const auto & joint : joints_) 
-  {
-    for (const auto & type : allowed_state_types_) 
-    {
-      names.push_back(joint.getName() + "/" + type);
-    }
-  }
-  return names;
-}
-
-inline JointHandle& JointManager::operator[](size_t index) 
-{
-  return joints_.at(index); 
-}
-
-inline const JointHandle& JointManager::operator[](size_t index) const
-{
-  return joints_.at(index); 
-}
 }
